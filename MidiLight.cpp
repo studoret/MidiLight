178943273d93d5531a847823a0373fa2040fcc9e
studoret <studoret@fake.bzh>
2016-04-27 15:59:19 +0200

    Update file header comments

--- MidiLight.cpp
+++ MidiLight.cpp
@@ -1,9 +1,28 @@
 /**
- * @file   MidiLight.cpp
- * @author S.Tudoret
- * @date   29/02/16
+ * @file    MidiLight.cpp
+ * @author  S.Tudoret
+ * @date    29/02/16
+ * @license MIT
  *
- * Copyright 2016 S. Tudoret.
+ * Copyright (c) 2016 S. Tudoret
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include "Arduino.h"
@@ -120,4 +139,26 @@ midiLight_read()
 
 out:
     return;
+}
+
+void
+midiLight_writeNoteOn(
+        byte channel,
+        byte pitch,
+        int velocity)
+{
+    serial->write(MIDI_STATUS_NOTE_ON | channel);
+    serial->write(pitch);
+    serial->write(velocity);
+}
+
+void
+midiLight_writeNoteOff(
+        byte channel,
+        byte pitch,
+        int velocity)
+{
+    serial->write(MIDI_STATUS_NOTE_OFF | channel);
+    serial->write(pitch);
+    serial->write(velocity);
 }
